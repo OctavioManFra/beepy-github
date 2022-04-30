d67b56ea8b9317a963ae3121ba8cb26325e14474
OctavioManFra <104694657+OctavioManFra@users.noreply.github.com>
2022-04-30 14:19:19 -0500

    BeePy
    Actividad de beepy

--- functions.py
+++ functions.py
@@ -52,3 +52,28 @@ def buberto_bunzales():
 		print("No sé qué es lo bueno :c")
 
 	return gusta_python
+
+
+def oktaviomanciya():
+	# Escribe tus datos
+	nombre = "octavio eduardo mancilla frausto"
+	edad   = 23
+	# Te gusta python
+	gusta_python = True
+	# Dinos 3 cosas que te gusten
+	gustos = [
+		 "Fraude Electoral",
+		 "Evasión de impuestos",
+		 "Santa Fe klan",
+	]
+	print(f"Mi nombre es: {Octavio Mancilla} y tengo {23} añitos")
+	print("Me gusta:")
+	for gusto in gustos:
+		print(gusto)
+	print()
+		
+	if gusta_python:
+		print("¡Y soy el exito! :D")
+	else:
+		print("No sé qué es lo bueno :c")
+	return gusta_python
